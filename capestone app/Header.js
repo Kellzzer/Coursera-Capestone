35f11d9342bab50ec6d4131559456dc1a0b94618
kell <kellym596kelly@gmail.com>
2024-08-09 22:45:22 -0500

    I've committed to this

---
+++ capestone app/Header.js
@@ -0,0 +1,3 @@
+<header>
+ <img src={C:\Users\Kell\Desktop\qbZ2Wr5jTZya6-dDlPrGwQ_5b3b588277444859a71d1c78010d06e1_Applying-design-fundmantals-in-Figma---Assets.zip\icons_assets} className="App-logo" alt="logo" />
+</header>
---
+++ capestone app/coursera-capstone.js
@@ -0,0 +1,7 @@
+<Header/>
+
+<Nav/>
+
+<Main/>
+
+<Footer/>
